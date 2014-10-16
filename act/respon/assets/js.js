de87af0c19cdd9b3ddfe1589ceb89ddd17ae03b5
piercalex <lijunal@126.com>
2014-10-16 17:35:36 +0800

    1016五点半汇总

--- act/respon/assets/js.js
+++ act/respon/assets/js.js
@@ -1,3 +1,4 @@
+//功能函数
 //scrollEffect 滚动函数
 function scrollEffect(obj,category,minLength,intervalTime,animateSpeed) {
     this.Ul = obj;
@@ -26,9 +27,82 @@ function scrollEffect(obj,category,minLength,intervalTime,animateSpeed) {
         }
     };
 };
+//popup
+function popupFunc(hd,bd,fd){
+    this.hd = hd;
+    this.bd = bd;
+    this.fd = fd;
+    popup = document.getElementById("popup-box"),
+        popupInside = popup.getElementsByClassName("popup-inside")[0],
+        popupTitle = popup.getElementsByClassName("title")[0],
+        popupBd = popup.getElementsByClassName("popup-bd")[0],
+        popupFd = popup.getElementsByClassName("popup-fd")[0],
+        popupClose = popup.getElementsByClassName("f-close")[0];
+    popupTitle.innerHTML = this.hd;
+    popupBd.innerHTML = this.bd;
+    if(popupFd.innerHTML == ""){
+        popupFd.parentNode.removeChild(popupFd);
+    }else{
+        popupFd.innerHTML = this.fd;
+    };
+};
+popupFunc.prototype = {
+    transmit : function(){
+        popup.style.display = "block";
+        popupInside.style.top = (document.documentElement.clientHeight - popupInside.offsetHeight) / 2 + "px";
+        popupClose.onclick = function(){
+            popup.style.display = "none";
+            popupTitle.innerHTML = "";
+            popupBd.innerHTML = "";
+            popupFd.innerHTML = "";
+        };
+    },
+    share : function(desc,url,pic,carsn){
+        popup.style.display = "block";
+        popupInside.style.top = (document.documentElement.clientHeight - popupInside.offsetHeight) / 2 + "px";
+        popupClose.onclick = function(){
+            popup.style.display = "none";
+            popupTitle.innerHTML = "";
+            popupBd.innerHTML = "";
+            popupFd.innerHTML = "";
+        };
+        var shareurl = popup.getElementsByClassName('share-ul')[0].getElementsByTagName('a');
+        for( i = 0; i < shareurl.length; i++){
+            shareurl[0].setAttribute('href','http://service.weibo.com/share/share.php?url=' + url + '&title=' + desc + '&pic=' + pic);
+            shareurl[1].setAttribute('href','http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=' + url + '&title=' + desc + '&pic=' + pic + '&summary=' + desc);
+            shareurl[2].setAttribute('href','http://share.v.t.qq.com/index.php?c=share&a=index&url=' + url + '&title=' + desc + '&pic=' + pic);
+        };
+    }
+};
+
+
+
+//应用函数
+(function(){
+    //底部栏关闭功能
+    var bmbar = document.getElementById("bmbar");
+    if(!bmbar) return false;
+    var bmbarClose = bmbar.getElementsByClassName("f-close")[0];
+    bmbarClose.onclick = function(){
+        bmbar.parentNode.removeChild(bmbar);
+    };
+})();
+
+
+
+
+
+$(function(){
+
+    var x = $("#morecars-ul").find("li").width() + 10;
+    var y = $("#morecars-ul").find("li").length;
+    $("#morecars-ul").css("width",x*y)
+    console.log(x +"_"+ y)
+})
 
 function response(){
-    $(".act-li-end").css("height",$("#act-li-box").find(".act-li").height())
+    $(".act-li-end").css("height",$("#act-li-box").find(".act-li").height());
+
 };
 
 window.onload = window.onresize = response;
